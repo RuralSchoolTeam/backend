c422c94240e6efb92f41ff7a05a45e7c1377d6b9
joshua-disney <joshua.a.disney@gmail.com>
2019-03-11 13:48:49 -0700

    Apply restricted middleware to prevent unauthorized access

--- users/usersModel.js
+++ users/usersModel.js
@@ -8,13 +8,7 @@ module.exports = {
 };
 
 function find() {
-  return db("users").select(
-    "id",
-    "username",
-    "password",
-    "authLevel",
-    "organization"
-  );
+  return db("users").select("username", "authLevel", "organization");
 }
 
 function findBy(filter) {
